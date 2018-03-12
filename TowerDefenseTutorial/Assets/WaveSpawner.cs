dfb31ac18b6b72d4380e213d7686acc88c7c60c3
Dragos Rogojan <dragos.rogojan@softvision.ro>
2018-03-12 16:39:37 +0200

    Wave Spawner

---
+++ TowerDefenseTutorial/Assets/WaveSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    private float countdown = 2f;
+    private int waveIndex = 0;
+
+    public Transform enemyPrefab;
+    public float timeBetweenWaves = 5f;
+    public Transform spwanPoint;
+
+    void Update()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spwanPoint.position, spwanPoint.rotation);
+    }
+}
